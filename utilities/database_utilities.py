df06bed2bc86a9598937d4f2867be46b3d485c53
mohitbhadauria10 <mohit.bhadauria@gmail.com>
2021-05-23 12:44:57 -0500

    Test

---
+++ utilities/database_utilities.py
@@ -0,0 +1,12 @@
+def create_database(database_path: str):
+    #TODO: generate the databse
+    pass
+
+def save_words_to_databse(database_path:str, words_list:list):
+    #TODO: add the code that reads the URL
+    pass
+
+def scrape_page(page_contents:str ):
+    #TODO: analyze the text
+    pass
+
